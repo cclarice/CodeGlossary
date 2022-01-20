68811ee1d151ca16fa925a6be2ac7c7e3205c4ec
cclarice <179danil@gmail.com>
2022-01-20 18:04:48 +0300

    Working on BaseBalloons.vue

---
+++ src/components/balloon/BaseBalloons.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="Balloons">
+    <div v-for="getBalloons" class="Balloon">
+      <div>
+
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BaseBalloons'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
