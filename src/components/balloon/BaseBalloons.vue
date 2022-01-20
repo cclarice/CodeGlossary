629748f6a87c34f1248bec8162d9e545bf2e6652
cclarice <179danil@gmail.com>
2022-01-20 18:22:32 +0300

    Working on BaseBalloon.vue

--- src/components/balloon/BaseBalloons.vue
+++ src/components/balloon/BaseBalloons.vue
@@ -1,7 +1,22 @@
 <template>
   <nav class="Balloons">
-    <div v-for="getBalloons" class="Balloon">
-      <div>
+    <div v-for="balloon of getBalloons" class="Balloon">
+      <div class="BalloonColumn BalloonLeft">
+        <img class="BalloonType" :src="getBalloonStatusIcon(balloon.type)">
+      </div>
+      <div class="BalloonColumn BalloonMain">
+        <div class="BalloonMainHeader">
+          <b></b>
+          <img class="BalloonSetting" alt="Settings"> <!-- todo add setting to the balloons -->
+        </div>
+        <p class="BalloonText">
+
+        </p>
+        <div class="BalloonActions">
+          
+        </div>
+      </div>
+      <div class="BalloonColumn BalloonRight">
 
       </div>
     </div>
@@ -17,5 +32,16 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.Balloons {
+  display: flex;
+  flex-flow: column-reverse;
+  gap: 30px;
+}
 
+.Balloon {
+  display: flex;
+  flex-flow: row;
+  max-width: 380px;
+  min-width: 240px;
+}
 </style>
