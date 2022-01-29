c647816e23ff4240f3ef24af9e1999cb018d52e5
cclarice <179danil@gmail.com>
2022-01-30 01:00:48 +0300

    Finished DebugPanel.vue

--- src/components/DebugPanel.vue
+++ src/components/DebugPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="DebugPanel" v-if="open" :style="{ left: `${x}px`, top: `${y}px` }">
+  <div class="DebugPanel" v-show="open" :style="{ left, top }">
     <div class="DebugPanelHeader">
       <span>Debug Panel</span>
       <BaseIcon class="DebugPanelButton"
@@ -7,16 +7,18 @@
                 @mousedown="mouseDownDebugButton"/>
     </div>
     <div class="DebugPanelBody">
-      <base-scrollable>
-        <code>
-          <slot/>
-        </code>
-      </base-scrollable>
+      <BaseScrollable>
+        <div class="ScrollableContent">
+          <code>
+            <slot>No data</slot>
+          </code>
+        </div>
+      </BaseScrollable>
     </div>
   </div>
   <BaseIcon class="DebugButton"
-            v-else
-            :style="{ left: `${x}px`, top: `${y}px` }"
+            v-show="!open"
+            :style="{ left, top }"
             :icon="require('@/assets/icons/interface/debug.svg')"
             @mousedown="mouseDownDebugButton"/>
 </template>
@@ -30,65 +32,54 @@ export default defineComponent({
   name: 'DebugPanel',
   data: () => ({
     open: false,
-    x: 0,
-    y: 0,
+    x: 0xFFFF,
+    y: 0xFFFF,
+    px: 0,
+    py: 0,
+    ox: 0,
+    oy: 0,
     prevEvent: null
   }),
+  computed: {
+    left (): string {
+      return `max(12px, min(${this.x}px, calc(100vw - 12px)))`
+    },
+    top (): string {
+      return `max(12px, min(${this.y}px, calc(100vh - 12px)))`
+    }
+  },
   components: { BaseIcon, BaseScrollable },
   mounted () {
-    console.log(this.$options.name, 'mounted')
-    const localX = localStorage.getItem('debugPositionX')
-    const localY = localStorage.getItem('debugPositionY')
+    const local = localStorage.getItem('debugPosition')
 
-    this.x = Number(localX) || window.innerWidth - 64
-    this.y = Number(localY) || window.innerHeight - 64
-  },
-  unmounted () {
-    console.log(this.$options.name, 'unmounted')
+    if (local) {
+      const pos = local.split(' ')
+      this.x = Number(pos[0])
+      this.y = Number(pos[1])
+    }
   },
   methods: {
+    moveDebug (event: MouseEvent): void {
+      this.x = event.pageX - this.ox
+      this.y = event.pageY - this.oy
+    },
     mouseDownDebugButton (event: MouseEvent): void {
-      const x = event.pageX
-      const y = event.pageY
-      const ox = event.offsetX - 12
-      const oy = event.offsetY - 12
+      this.px = event.pageX
+      this.py = event.pageY
+      this.ox = event.offsetX - 12
+      this.oy = event.offsetY - 12
 
-      const moveDebug = (event: MouseEvent): void => {
-        if (event.pageX) { this.x = event.pageX - ox }
-        if (event.pageY) { this.y = event.pageY - oy }
-      }
       document.addEventListener('mouseup', (event: MouseEvent): void => {
-        let px = event.pageX
-        let py = event.pageY
-
-
-        if (px > window.innerWidth - 13) {
-          px = window.innerWidth - 13
-        } else if (px < 13) {
-          px = 13
-        }
-        if (py > window.innerHeight - 13) {
-          py = window.innerHeight - 13
-        } else if (py < 13) {
-          py = 13
-        }
-
-        if (event.pageX === x && event.pageY === y) {
+        if (this.px === event.pageX && this.py === event.pageY) {
           this.open = !this.open
-        } else {
-          if (px) {
-            localStorage.setItem('debugPositionX', String(px - ox))
-            this.x = px - ox
-          }
-          if (py) {
-            localStorage.setItem('debugPositionY', String(py - oy))
-            this.y = py - oy
-          }
+        } else if (event.pageX && event.pageY) {
+          this.x = event.pageX - this.ox
+          this.y = event.pageY - this.oy
+          localStorage.setItem('debugPosition', `${this.x} ${this.y}`)
         }
-        document.removeEventListener('mousemove', moveDebug)
+        document.removeEventListener('mousemove', this.moveDebug)
       }, { once: true })
-
-      document.addEventListener('mousemove', moveDebug)
+      document.addEventListener('mousemove', this.moveDebug)
     }
   }
 })
@@ -102,8 +93,8 @@ export default defineComponent({
   border-radius: 5px;
   user-select: none;
   transform: translate(-50%, -50%);
-  opacity: 0.25;
-  transition: opacity 0.25s;
+  opacity: 0.33;
+  transition: opacity .2s;
   &:hover {
     opacity: 1;
   }
@@ -116,8 +107,6 @@ export default defineComponent({
   transform: translate(calc(-100% + 13px), 0);
   max-width: 325px;
   max-height: 617px;
-  min-width: 160px;
-  min-height: 325px;
   background-color: var(--panel-background);
   border: 1px solid var(--panel-border);
   &Button {
@@ -133,8 +122,8 @@ export default defineComponent({
     max-height: 24px;
     background-color: var(--popup-header);
   }
-  opacity: 0.25;
-  transition: opacity 0.25s;
+  opacity: 0.33;
+  transition: opacity .2s;
   &:hover {
     opacity: 1;
   }
@@ -144,5 +133,11 @@ export default defineComponent({
     -moz-user-drag: none;
     -o-user-drag: none;
   }
+  .BaseScrollable {
+    display: flex;
+  }
+}
+.ScrollableContent {
+  padding: 6px;
 }
 </style>
