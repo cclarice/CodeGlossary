3d4f7ada27f65b4ea103c482254231962e094dea
oredan <179danil@gmail.com>
2022-03-22 16:20:21 +0300

    new router branch added

---
+++ src/router/routes/develop.ts
@@ -0,0 +1,35 @@
+import { RouteRecordRaw } from 'vue-router'
+import layouts from '@/layouts/layouts'
+
+const develop: Array<RouteRecordRaw> = [
+  {
+    path: '/dev',
+    name: 'Develop',
+    component: () => import('@/views/Home.vue'),
+    meta: {
+      layout: layouts.MainLayout,
+      stats: {
+        creator: '',
+        createdDate: new Date(1647868571807)
+      },
+      parent: '/'
+    }
+  },
+  {
+    path: '/dev/interface',
+    name: 'Interface',
+    component: () => import('@/views/develop/Interface.vue'),
+    meta: {
+      layout: layouts.MainLayout,
+      stats: {
+        creator: 'oredan',
+        creatorUrl: '',
+        createdDate: new Date(1647892462691),
+        updatedDate: new Date(1647892462691)
+      },
+      parent: '/'
+    }
+  }
+]
+
+export default develop
