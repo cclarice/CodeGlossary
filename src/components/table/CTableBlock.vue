30fa60ffdaecd626dfd355df5c8904515fe57662
oredan <179danil@gmail.com>
2022-03-23 15:33:27 +0300

    Interface.vue and CTable.vue developing

---
+++ src/components/table/CTableBlock.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="props.tag">
+
+  </component>
+</template>
+
+<script setup lang="ts">
+import { Component } from 'vue'
+import { TableBlock } from '@/components/table/TTable'
+
+interface Props {
+  tag: 'thead' | 'tbody' | 'tfoot'
+  block: TableBlock
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
