86da618664b7f2abfc3c97e73a2b8467e2e2deca
oredan <179danil@gmail.com>
2022-03-23 15:31:40 +0300

    Table added

---
+++ src/components/table/TTable.ts
@@ -0,0 +1,30 @@
+import { Component } from 'vue'
+
+interface TableCellAdvanced {
+  content?: string | Component
+  column?: number
+  row?: number
+}
+
+type TableCell = TableCellAdvanced | string
+
+interface TableColumnGroup {
+  span: number
+  class: string
+  static: boolean
+}
+type TableColumnGroups = Array<TableColumnGroup>
+
+type TableRow = Array<TableCell>
+export type TableBlock = Array<TableRow>
+type TableHead = TableBlock
+type TableBody = TableBlock
+type TableFoot = TableBlock
+
+export default interface Table {
+  head?: TableHead
+  body?: TableBody
+  foot?: TableFoot
+  caption?: string
+  column_group?: TableColumnGroups
+}
