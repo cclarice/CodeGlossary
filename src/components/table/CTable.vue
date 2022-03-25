5bd9f172d2095b1a46d42b7aacb8d19aee445a3e
oredan <179danil@gmail.com>
2022-03-25 12:57:45 +0300

    CTable.vue developing

--- src/components/table/CTable.vue
+++ src/components/table/CTable.vue
@@ -1,63 +1,76 @@
 <template>
   <table v-if="props.table">
-    <caption />
-    <colgroup>
+    <caption v-if="props.table.caption" />
+    <colgroup v-if="props.table.column_group">
       <col>
     </colgroup>
-    <c-table-block />
-    <thead v-if="props.table.head">
-      <tr
-        v-for="table_row of props.table.head"
-        :key="table_row"
-      >
-        <td
-          v-for="table_cell of table_row"
-          :key="table_cell"
+    <c-table-block
+      v-for="(block, tag) in { head: props.table.head, body: props.table.body, foot: props.table.foot }"
+      :key="block"
+      :tag="`t${tag}`"
+      :block="block"
+    />
+    <!--
+      <thead v-if="props.table.head">
+        <tr
+          v-for="table_row of props.table.head"
+          :key="table_row"
         >
-          {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
-        </td>
-      </tr>
-    </thead>
-    <tbody v-if="props.table.body">
-      <tr
-        v-for="table_row of props.table.body"
-        :key="table_row"
-      >
-        <td
-          v-for="table_cell of table_row"
-          :key="table_cell"
+          <td
+            v-for="table_cell of table_row"
+            :key="table_cell"
+          >
+            {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
+          </td>
+        </tr>
+      </thead>
+      <tbody v-if="props.table.body">
+        <tr
+          v-for="table_row of props.table.body"
+          :key="table_row"
         >
-          {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
-        </td>
-      </tr>
-    </tbody>
-    <tfoot v-if="props.table.foot">
-      <tr
-        v-for="table_row of props.table.foot"
-        :key="table_row"
-      >
-        <td
-          v-for="table_cell of table_row"
-          :key="table_cell"
+          <td
+            v-for="table_cell of table_row"
+            :key="table_cell"
+          >
+            {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-if="props.table.foot">
+        <tr
+          v-for="table_row of props.table.foot"
+          :key="table_row"
         >
-          {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
-        </td>
-      </tr>
-    </tfoot>
+          <td
+            v-for="table_cell of table_row"
+            :key="table_cell"
+          >
+            {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
+          </td>
+        </tr>
+      </tfoot>
+    -->
   </table>
 </template>
 
 <script setup lang="ts">
-import Table from '@/components/table/TTable'
+import Table from '@/models/Table'
 import CTableBlock from '@/components/table/CTableBlock.vue'
 
 interface Props {
   table: Table
+  sortable?: boolean
 }
 
 const props = defineProps<Props>()
+const sortable = props.sortable || false
 </script>
 
 <style lang="scss" scoped>
 
+
+table {
+  border-collapse: collapse;
+}
 </style>
--- src/components/table/CTableBlock.vue
+++ src/components/table/CTableBlock.vue
@@ -4,18 +4,25 @@
       v-for="table_row of props.block"
       :key="table_row"
     >
-      <td
+      <component
+        :is="(props.tag === 'thead' ? 'th' : 'td')"
         v-for="table_cell of table_row"
         :key="table_cell"
       >
-        {{ typeof table_cell === 'string' ? table_cell : table_cell.content }}
-      </td>
+        {{ typeof table_cell === 'string' ? table_cell :
+          (typeof table_cell.content === 'string' ? table_cell.content : '') }}
+        <component
+          :is="table_cell.content"
+          v-if="table_cell.content instanceof Component"
+        />
+      </component>
     </tr>
   </component>
 </template>
 
 <script setup lang="ts">
-import { TableBlock } from '@/components/table/TTable'
+import { TableBlock } from '@/models/Table'
+import { Component } from 'vue'
 
 interface Props {
   tag: 'thead' | 'tbody' | 'tfoot'
@@ -26,5 +33,20 @@ const props = defineProps<Props>()
 </script>
 
 <style lang="scss" scoped>
-
+td, th {
+  border: 1px solid var(--table-border);
+}
+thead {
+  background-color: var(--panel-background);
+  th {
+    padding: 3px 5px 2px;
+    text-align: center;
+  }
+}
+tbody {
+  background-color: var(--main-background);
+  td {
+    padding: 1px 5px;
+  }
+}
 </style>
