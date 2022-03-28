82fe27708471f8f4e409ac209b546650f4d5002c
oredan <179danil@gmail.com>
2022-03-28 18:29:20 +0300

    MainBar developing

--- src/layouts/main-components/MainBar.vue
+++ src/layouts/main-components/MainBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav
     class="main-bar"
-    :class="`main-bar_${position}`"
+    :class="`main-bar_${props.position}`"
   >
     <div class="main-bar__left">
       <slot name="bar_left" />
@@ -32,18 +32,43 @@ defineProps<{ position: 'top' | 'bottom' | 'left' | 'right' }>()
     border-bottom-width: 1px;
   }
 
+  &_top, &_bottom {
+    &:deep(.main-bar-button) {
+      height: 100%;
+      padding: 0 10px;
+    }
+  }
+
   &_left, &_right {
     width:             21px;
     border-left-width: 1px;
     writing-mode:      vertical-rl;
+
+    &:deep(.main-bar-button) {
+      width: 100%;
+      padding: 10px 0;
+    }
   }
 
   &_left {
     transform: rotate(180deg);
+    &:deep(.main-bar-button__icon) {
+      transform: rotate(180deg);
+    }
   }
 
   &_bottom {
     border-top-width: 1px;
+  }
+
+  &__left,
+  &__right {
+    display: flex;
+    flex-grow: 1;
+  }
+
+  &__right {
+    flex-flow: row-reverse;
   }
 }
 </style>
