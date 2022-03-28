6e61c505d83f201420ac466b4c285ebcf91fc53c
oredan <179danil@gmail.com>
2022-03-28 18:29:42 +0300

    MainBarButton developing

--- src/layouts/main-components/MainBarButton.vue
+++ src/layouts/main-components/MainBarButton.vue
@@ -4,8 +4,8 @@
       src=""
       alt=""
     >
-    <span />
-  </button>
+    <span class="main-bar-button__name"> {{ props.tool.name }} </span>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -13,5 +13,24 @@
 </script>
 
 <style lang="scss" scoped>
+.main-bar-button {
+  display:          flex;
+  align-items:      center;
+  gap:              4px;
+  background-color: transparent;
+  user-select: none;
 
+  &:hover {
+    background-color: var(--stripe-hovered-background);
+  }
+
+  .active {
+    background-color: var(--stripe-selected-background);
+  }
+
+  &__icon {
+    filter: var(--icon-filter);
+    user-select: none;
+  }
+}
 </style>
